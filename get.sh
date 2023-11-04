32567f3d9a8bdf318d4fea5fd74fc209cb706a96
Tarsis <br.tmvdl@gmail.com>
2023-11-04 11:36:21 -0300

    Adding script files

---
+++ get.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/sh
+
+. .env
+
+. datetime.sh
+
+API_PATH="${1}"
+
+curl -X GET \
+  -H "X-CB-ACCESS-KEY: ${ACCESS_KEY}" \
+  -H "X-CB-ACCESS-PASSPHRASE: ${PASSPHRASE}" \
+  -H "X-CB-ACCESS-SIGNATURE: ${SIGNATURE}" \
+  -H "X-CB-ACCESS-TIMESTAMP: ${timestamp}" \
+  -H "Content-Type: application/json" \
+  "https://api.prime.coinbase.com/v1/${API_PATH}"
